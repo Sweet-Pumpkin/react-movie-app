c4953d8cafd728a06c21cd4aafb90e43817f3107
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-05-30 11:55:58 +0900

    feat: React Router

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,15 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export default function Movie({ coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt="movie_image"/>
-      <h2>{title}</h2>
+      <h2>
+        <Link to="/movie">
+          {title}
+        </Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         { genres.map(g => 
